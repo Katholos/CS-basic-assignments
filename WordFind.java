d9fc2cad1671ee51c02da7fbf74d12d56e2dd12a
Matt Raetano <matthew.raetano@gmail.com>
2017-07-24 09:33:36 -0400

    Update WordFind.java

--- WordFind.java
+++ WordFind.java
@@ -21,10 +21,13 @@ public class WordFind {
 			
 			System.out.print("Enter a three-letter word: ");
 			String word1 = kbd.next();
+		
 			System.out.print("Enter a four-letter word: ");
 			String word2 = kbd.next();
+		
 			System.out.print("Enter a five-letter word: ");
 			String word3 = kbd.next();
+		
 			System.out.print("Enter another four-letter word: ");
 			String word4 = kbd.next();
 			
