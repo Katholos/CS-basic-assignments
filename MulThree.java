622c2ebbcacfb9517361aa7a2412a71cb5b19f0a
Matt Raetano <matthew.raetano@gmail.com>
2017-07-24 09:24:36 -0400

    Update MulThree.java

--- MulThree.java
+++ MulThree.java
@@ -1,5 +1,12 @@
 //Matt Raetano
-//program takes three whole numbers from the user and returns the product of them
+//Revise the program from the tutorial so that it multiplies three numbers together, rather than simply adding two numbers.
+//Begin by copying the program code from the tutorial and pasting it into proper place in this new program file.
+//The tutorial program prints messages to the screen telling the user that the program adds two numbers, and then tells them to enter two numbers. We need to change the first few print statements so they display the correct instructions to the user - that is, we need to be telling the user that the program multiplies three numbers, and tell them to enter three numbers.
+//Add one more variable to the program to hold the extra number.
+//Add the extra statement needed to get the additional number from the program's user and place it into the extra variable.
+//Change the final message that is displayed to the screen so that it correctly says that it has multiplied three, and not added two numbers.
+//Change the final System.out.println statement so that it performs the correct arithmetic.
+
 import java.util.*;
 
 public class MulThree {
