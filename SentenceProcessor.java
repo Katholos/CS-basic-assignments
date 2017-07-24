6d6a3b1f2e40045bf85b56b3162c1c7230e21446
Matt Raetano <matthew.raetano@gmail.com>
2017-07-24 09:41:37 -0400

    Update SentenceProcessor.java

--- SentenceProcessor.java
+++ SentenceProcessor.java
@@ -1,5 +1,12 @@
 //Matt Raetano
-//Program gets a one-line sentence from the user, and then outputs one of several responses
+//Write a program that gets a one-line sentence from the user, and then outputs one of several responses, as follows:
+//If the sentence ends with a question mark '?' and the sentence contains an even number of characters, output to the screen the words: That's an even question.
+//If the sentence ends with a question mark '?' and the sentence contains an odd number of characters, output to the screen the words: That's an odd question.
+//If the sentence ends with an exclamation mark '!', output to the screen the word: Wow!
+//For all other cases, output the words: You always say, " 
+//followed by the sentence entered by the user. In your output, display double quotes around the user's sentence.
+//When testing your program, make sure that you are checking for the correct conditions. For example, make sure your program still works correctly if there is a question mark in the middle of the sentence, rather than at the end.
+
 import java.util.*;
 
 public class SentenceProcessor {
