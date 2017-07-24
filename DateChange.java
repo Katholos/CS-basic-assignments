6d09e2219d5525bf4d9a9fe94d631b2540db4456
Matt Raetano <matthew.raetano@gmail.com>
2017-07-24 09:35:52 -0400

    Update DateChange.java

--- DateChange.java
+++ DateChange.java
@@ -1,5 +1,9 @@
 //Matt Raetano
-//This program will receive an inputed date and then converts it into European style
+//In the United States, we commonly express dates by listing the month, then the day then the year, all separated by slash marks. In Europe, however, dates are commonly expressed by listing the day first, then the month, then the year, all separated by periods.
+//Write a JAVA application program that will get a line of text containing a date in US form from the user, and then change this date to European form.
+//Begin by asking the user to enter a date in the form of month/day/year. Store this date in a String variable.
+//Next, use the appropriate String methods to swap the month and day parts of the date, and replace the slash marks with periods. Print the revised String to the screen.
+
 import java.util.*;
 
 public class DateChange {
