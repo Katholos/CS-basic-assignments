09d915fe2c379f92f34d54651fb759a022010b69
Matt Raetano <matthew.raetano@gmail.com>
2017-07-24 09:25:39 -0400

    Update SimpleMath.java

--- SimpleMath.java
+++ SimpleMath.java
@@ -1,5 +1,12 @@
 //Matt Raetano
-//Program finds the sum, difference, and product of two inputted integers
+//Write a Java application program that reads two floating point numbers from the keyboard, and then prints their sum, difference, and product.
+//Your program should perform the following steps, in this order:
+//Print a prompt to the screen telling the user that you want him/her to enter two real numbers. 
+//Get the two numbers from the keyboard and store them into two floating point variables in your program.
+//Perform your calculations. Store the answer to each calculation in a separate floating point variable.
+//Print out the three answers to the screen. Before each answer, be sure to print a simple message to the user so they will know what the number means.
+//In total, you will need five floating point variables, and one keyboard object variable for this program. Make sure you choose names for your variables that indicate what the variables might be holding.
+
 import java.util.*;
 
 public class SimpleMath {
