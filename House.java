2fc3df56619568e229f6efc310b5f931c774f56f
Matt Raetano <matthew.raetano@gmail.com>
2017-07-24 09:23:10 -0400

    Update House.java

--- House.java
+++ House.java
@@ -1,5 +1,5 @@
 //Matt Raetano
-//Program manipulates the println() method to create a printed house
+//The house is printed by using several Java System.out.println statements - one per line. Each print statement should print some spaces, and some asterisks. This program does not need to get any input from the user, therefore the user will not be required to type anything into the program while the program is executing. Likewise, you will not need the import statement at the top of the program since no keyboard input is required.
 public class House {
 
 	public static void main(String[] args) {
