ea9c5297d46f0a541b08cb0c84c8809424461d89
Matt Raetano <matthew.raetano@gmail.com>
2017-07-27 10:29:20 -0400

    Update Tutorial1.java

--- Tutorial1.java
+++ Tutorial1.java
@@ -9,14 +9,12 @@ public class Tutorial1 {
 		Scanner kbd = new Scanner(System.in);
 		int n1, n2;
 
-		System.out.println("Hello out there.");
-		System.out.println("I will add two numbers for you.");
-		System.out.println("Enter two whole numbers on a line:");
+		System.out.print("Hello out there." +  "I will add two numbers for you." + "Enter two whole numbers on a line: ");
 
 		n1 = kbd.nextInt();
 		n2 = kbd.nextInt();
 
-		System.out.println("The sum of those two numbers is");
+		System.out.print("The sum of those two numbers is: ");
 		System.out.println( n1 + n2 );
 
 		kbd.close();
